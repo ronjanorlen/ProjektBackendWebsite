93e3edcfbc29760d322289f690c43fd63a914720
Ronja Norlén <rono2300@student.miun.se>
2024-05-10 15:04:30 +0200

    Logga in, formulär för att skapa användare, formulär för att redigera/lägga till maträtt

---
+++ src/js/login.js
@@ -0,0 +1,47 @@
+"use strict";
+
+import { url } from "./main"; // Importera API
+import { errorMsg } from "./main"; // Importera felmeddelande
+
+export async function loginUser() {
+    const username = document.getElementById("username").value; // Hämtar användarnamn från formulär
+    const password = document.getElementById("password").value; // Hämtar lösenord från formulär
+
+    try {
+
+        // Skapa nytt objekt för användare
+        const loginData = {
+            username: username,
+            password: password
+        }
+
+        // POST-anrop med fetch API till webbtjänsten, ta med objekt som skapats för användare
+        const response = await fetch(`${url}login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData)
+        });
+        
+        const data = await response.json(); // Invänta svar och konvertera till json
+
+        // Kontroll om svaret är lyckat eller inte
+        if (!response.ok) {
+            console.error("Inloggningsfel: ", data.error); 
+            errorMsg.style.display = "flex"; 
+            errorMsg.innerHTML = data.error;
+            return;
+
+            // Vid lyckat svar
+        } else {
+            localStorage.setItem("JWT", data.response.token); // Spara JWT-token i localStorage
+            localStorage.setItem("username", username); // Spara användarnamnet i localStorage
+            window.location.href = "/admin.html"; // Omdirigera användaren till admin-sida
+        }
+
+        // Fångar upp ev fel
+    } catch (error) {
+        console.error("Något gick fel vid inloggning: ", error);
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,45 @@
 
 /* Moduler */
 import { getMenu } from "./menu"; // Importera hämta meny
+import { loginUser } from "./login"; // Importera logga in-funktion
+
 
 /* Variabler */
 export const url = "https://projektbackendwebservice.onrender.com/api/"; // API, exporteras
 export const menuContainer = document.getElementById("menu-container"); // Meny-container, exporteras
+export let errorMsg = document.getElementById("error-message"); // Felmeddelande
+
+const loginContainer = document.getElementById("login"); // Logga in-formulär
+const loginBtn = document.getElementById("submit-login"); // Logga in-knapp
+
 
 /* När sidan laddas */
 window.onload = init();
 
 function init() {
-    // Kontrollera om meny-container finns, isf kör getMenu-funktion
+    // Kontrollera om meny-container finns
     if (menuContainer) {
         getMenu();
     }
+
+    // Kontrollera om logga in-container finns
+    if (loginContainer) {
+        loginBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            loginUser();
+        });
+    }
+        // Kontroll om besökaren står på skyddad sida eller ej
+        if (["/admin.html", "/adminmenu.html"].includes(window.location.pathname)) {
+            const token = localStorage.getItem("JWT"); // Hämta token från localStorage
+            // Kontroll om token saknas
+            if (!token) {
+                alert("Du är inte inloggad eller din session har gått ut. Vänligen logga in igen."); // Alert-ruta med felmeddelande
+                window.location.href = "login.html"; // Omdirigerar till inloggningssidan
+            } else {
+            
+            };
+        }
 }
 
 
